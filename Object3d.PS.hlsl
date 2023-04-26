25fa04dc96fd919a2348a7f3aadc271d9ec62f3d
知己 山川 <tomoki10160613@gmail.com>
2023-04-26 15:16:54 +0900

    a

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -2,3 +2,13 @@ float4 main() : SV_TARGET
 {
 	return float4(1.0f, 1.0f, 1.0f, 1.0f);
 }
+
+struct PixelShaderOutput {
+	float32_t4 color : SVTARGET0;
+};
+
+PixelShaderOutput main() {
+	PixelShaderOutput output;
+	output.color = float32_t4(1.0f, 1.0f, 1.0f, 1.0f);
+	return output;
+}
