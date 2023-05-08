bc68a879c129b36d3d2f175d8ef8504868981e4b
知己 山川 <tomoki10160613@gmail.com>
2023-05-08 14:36:29 +0900

    a

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,18 +1,18 @@
-struct Material {
-	float32_t4 color;
-};
-
-ConstantBuffer<Material>gMaterial : register(b0);
-struct PixelShaderOutput {
-	float32_t4 color : SV_TARGET0;
-};
-
+//struct Material {
+//	float32_t4 color;
+//};
+//
+//ConstantBuffer<Material>gMaterial : register(b0);
 //struct PixelShaderOutput {
 //	float32_t4 color : SV_TARGET0;
 //};
 
+struct PixelShaderOutput {
+	float32_t4 color : SV_TARGET0;
+};
+
 PixelShaderOutput main() {
 	PixelShaderOutput output;
-	output.color = gMaterial.cilor;
+	output.color = float32_t4(1.0,1.0,1.0,1.0);
 	return output;
 }
