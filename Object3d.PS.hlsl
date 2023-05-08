7258bb13a91e84ba833b76ba3f6c33d61f85ff21
知己 山川 <tomoki10160613@gmail.com>
2023-05-08 11:48:10 +0900

    a

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -7,9 +7,9 @@ struct PixelShaderOutput {
 	float32_t4 color : SV_TARGET0;
 };
 
-struct PixelShaderOutput {
-	float32_t4 color : SV_TARGET0;
-};
+//struct PixelShaderOutput {
+//	float32_t4 color : SV_TARGET0;
+//};
 
 PixelShaderOutput main() {
 	PixelShaderOutput output;
