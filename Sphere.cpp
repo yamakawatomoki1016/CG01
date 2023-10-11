2fed599fe340013f0e0029bc22abfc2167fee899
知己 山川 <tomoki10160613@gmail.com>
2023-10-11 13:02:12 +0900

    Sphere.h,cpp追加

--- Sphere.cpp
+++ Sphere.cpp
@@ -20,12 +20,42 @@ void Sphere::Draw(const Vector4& material, const Matrix4x4& wvpData)
 		for (lonIndex = 0; lonIndex < kSubdivision; lonIndex) {
 			uint32_t start = (latIndex * kSubdivision + lonIndex) * 6;
 			float lon = lonIndex * kLonEvery;
-			//頂点データを入力する
+			//頂点データを入力する。基準点a
 			sprite_->vertexDataSprite_[start].position.x = cos(lat) * cos(lon);
 			sprite_->vertexDataSprite_[start].position.y = sin(lat);
 			sprite_->vertexDataSprite_[start].position.z = cos(lat) * sin(lon);
 			sprite_->vertexDataSprite_[start].position.w = 1.0f;
-			//sprite_->vertexDataSprite_[start].texcoord = 
+			sprite_->vertexDataSprite_[start].texcoord = { 0.0f,1.0f };
+
+			sprite_->vertexDataSprite_[start + 1].position.x = cos(lat) * cos(lon);
+			sprite_->vertexDataSprite_[start + 1].position.y = sin(lat);
+			sprite_->vertexDataSprite_[start + 1].position.z = cos(lat) * sin(lon);
+			sprite_->vertexDataSprite_[start + 1].position.w = 1.0f;
+			sprite_->vertexDataSprite_[start + 1].texcoord = { 0.2f,0.8f };
+
+			sprite_->vertexDataSprite_[start + 2].position.x = cos(lat) * cos(lon);
+			sprite_->vertexDataSprite_[start + 2].position.y = sin(lat);
+			sprite_->vertexDataSprite_[start + 2].position.z = cos(lat) * sin(lon);
+			sprite_->vertexDataSprite_[start + 2].position.w = 1.0f;
+			sprite_->vertexDataSprite_[start + 2].texcoord = { 0.4f,0.6f };
+			
+			sprite_->vertexDataSprite_[start + 3].position.x = cos(lat) * cos(lon);
+			sprite_->vertexDataSprite_[start + 3].position.y = sin(lat);
+			sprite_->vertexDataSprite_[start + 3].position.z = cos(lat) * sin(lon);
+			sprite_->vertexDataSprite_[start + 3].position.w = 1.0f;
+			sprite_->vertexDataSprite_[start + 3].texcoord = { 0.6f,0.4f };
+
+			sprite_->vertexDataSprite_[start + 4].position.x = cos(lat) * cos(lon);
+			sprite_->vertexDataSprite_[start + 4].position.y = sin(lat);
+			sprite_->vertexDataSprite_[start + 4].position.z = cos(lat) * sin(lon);
+			sprite_->vertexDataSprite_[start + 4].position.w = 1.0f;
+			sprite_->vertexDataSprite_[start + 4].texcoord = { 0.8f,0.2f };
+
+			sprite_->vertexDataSprite_[start + 5].position.x = cos(lat) * cos(lon);
+			sprite_->vertexDataSprite_[start + 5].position.y = sin(lat);
+			sprite_->vertexDataSprite_[start + 5].position.z = cos(lat) * sin(lon);
+			sprite_->vertexDataSprite_[start + 5].position.w = 1.0f;
+			sprite_->vertexDataSprite_[start + 5].texcoord = { 1.0f,0.0f };
 
 			*triangle_->materialData_ = material;
 			*triangle_->wvpData_ = wvpData;
@@ -36,7 +66,7 @@ void Sphere::Draw(const Vector4& material, const Matrix4x4& wvpData)
 			directXManager_->GetCommandList()->SetGraphicsRootConstantBufferView(1, triangle_->wvpResource_->GetGPUVirtualAddress());
 			directXManager_->GetCommandList()->SetGraphicsRootDescriptorTable(2, myEngine_->textureSrvHandleGPU_);
 			//描画！(DrawCall/ドローコール)・3頂点で1つのインスタンス。インスタンスについては今後
-			directXManager_->GetCommandList()->DrawInstanced(3, 1, 0, 0);
+			directXManager_->GetCommandList()->DrawInstanced(6, 1, 0, 0);
 		}
 	}
 }
@@ -50,7 +80,7 @@ void Sphere::Finalize()
 
 void Sphere::CreateVertexData()
 {
-	triangle_->vertexResource_ = triangle_->CreateBufferResource(directXManager_->GetDevice(), sizeof(VertexData) * 3);
+	triangle_->vertexResource_ = triangle_->CreateBufferResource(directXManager_->GetDevice(), sizeof(VertexData) * 6);
 	//リソースの先頭のアドレスから使う
 	triangle_->vertexBufferView_.BufferLocation = triangle_->vertexResource_->GetGPUVirtualAddress();
 	//使用するリソースのサイズは頂点3つ分のサイズ
